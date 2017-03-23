aa6d606d8f9379fde287b73937dfe2f5add1f16f
Berezhnyuk <xboax@ukr.net>
2017-03-23 12:06:18 +0200

    fixed

--- main.js
+++ main.js
@@ -17,13 +17,18 @@ submitFunction3 = () => {
 	document.getElementById("result3").setAttribute("value", result3.toString());
 };
 submitFunction4 = () => {
-	let a = document.getElementById("a").value;
-	let b = document.getElementById("b").value;
-	let c = document.getElementById("c").value;
+	let a = document.getElementById("a2").value;
+	let b = document.getElementById("b2").value;
+	let c = document.getElementById("c2").value;
 	let D = (b*b) - (4*a*c);
+	if (D<0){
+	document.getElementById("result4").setAttribute("value", "Дискримінант <0,дійсних коренів немає");
+	document.getElementById("result5").setAttribute("value"," " );
+	}
+	else{
 	let K= Math.pow(D,1/2);
 	let result4 = (-b-K)/(2*a);
 	let result5 = (-b+K)/(2*a);
 	document.getElementById("result4").setAttribute("value", result4.toString());
-	document.getElementById("result5").setAttribute("value", result5.toString());
+	document.getElementById("result5").setAttribute("value", result5.toString());}
 };
